f956d11baa78815fcf74cc1ba2425a3f0ccc75c7
Teodoro Obando <teo.obando09@gmail.com>
2022-03-14 23:10:14 -0600

    Updating some code changes
    Play video and pause video is working

--- assets/MediaPlayer.js
+++ assets/MediaPlayer.js
@@ -1,13 +1,25 @@
 function MediaPlayer(config) {
     this.media = config.el
     this.plugins = config.plugins || []
+    this._initPlugins()
 }
 
+MediaPlayer.prototype._initPlugins = function() {
+    this.plugins.forEach(plugin => {
+        plugin.run(this)
+    })
+}
 MediaPlayer.prototype.play = function() {
     this.media.play()
 }
 MediaPlayer.prototype.pause = function() {
     this.media.pause()
+}
+MediaPlayer.prototype.mute = function() {
+    this.media.muted = true
+}
+MediaPlayer.prototype.unmute = function() {
+    this.media.muted = false
 }
 MediaPlayer.prototype.togglePlay = function() {
     if(this.media.paused) {
--- assets/plugins/AutoPlay.js
+++ assets/plugins/AutoPlay.js
@@ -1,5 +1,9 @@
 function AutoPlay() {
     //Write something amazing here
 }
+AutoPlay.prototype.run = function(player) {
+    player.mute()
+    player.play()
+}
 
 export default AutoPlay
