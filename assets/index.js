b7589398242fdc4f0768775dddded4b6d8e7ddb3
Teodoro Obando <teo.obando09@gmail.com>
2022-05-02 00:50:25 -0600

    Creating Ads's plugin
    Not implemented on view yet

--- assets/index.js
+++ assets/index.js
@@ -1,6 +1,7 @@
 import MediaPlayer from './MediaPlayer.js';
 import AutoPlay from './plugins/AutoPlay.js';
 import AutoPause from './plugins/AutoPause.ts';
+import Ads from './plugins/Ads';
 
 const video = document.querySelector('video')
 const player = new MediaPlayer({ 
@@ -8,6 +9,7 @@ const player = new MediaPlayer({
     plugins: [
         new AutoPlay(),
         new AutoPause(),
+        new Ads(),
     ] 
 })
 
